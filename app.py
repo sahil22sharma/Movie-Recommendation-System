b780dfd4985d714945fca616d7ff74c5029dc1d1
Sahil Sharma <104979660+sahil22sharma@users.noreply.github.com>
2024-06-29 12:19:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import pickle
 import streamlit as st
 st.set_page_config(layout="wide")
 import pandas as pd
-import requests
+import requests 
 
+rating = [1,2,3,4,5]
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=d08164a07d179c1692fdf3d743b939e9&language=en-US'.format(movie_id))
     data = response.json()
@@ -14,7 +15,7 @@ def fetch_poster(movie_id):
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)),reverse=True,key = lambda x: x[1])[1:6]
+    movies_list = sorted(list(enumerate(distances)),reverse=True,key = lambda x: x[1])[1:26]
 
     recommended_movies = []
     recommended_movies_posters = []
@@ -33,23 +34,102 @@ st.title("Movie Recommender System")
 selected_movie_name = st.selectbox(
     "Type or select a movie from the dropdown",
     movies['title'].values)
-    # index=None)
+ratings = st.selectbox(
+    "Enter Ratings",
+    rating,
+    placeholder="Enter the ratings of the movie",
+    index=None
+)
 
 if st.button('Show Recommendation'):
-    name,poster = recommend(selected_movie_name)
-    col1, col2, col3, col4, col5 = st.columns(5,gap='medium')
-    with col1:
-        st.text(name[0])
-        st.image(poster[0])
-    with col2:
-        st.text(name[1])
-        st.image(poster[1])
-    with col3:
-        st.text(name[2])
-        st.image(poster[2])
-    with col4:
-        st.text(name[3])
-        st.image(poster[3])
-    with col5:
-        st.text(name[4])
-        st.image(poster[4])
+    if ratings == 5:
+        name,poster = recommend(selected_movie_name)
+        col1, col2, col3, col4, col5 = st.columns(5,gap='medium')
+        with col1:
+            st.text(name[0])
+            st.image(poster[0])
+        with col2:
+            st.text(name[1])
+            st.image(poster[1])
+        with col3:
+            st.text(name[2])
+            st.image(poster[2])
+        with col4:
+            st.text(name[3])
+            st.image(poster[3])
+        with col5:
+            st.text(name[4])
+            st.image(poster[4])
+    elif ratings == 4:
+        name,poster = recommend(selected_movie_name)
+        col1, col2, col3, col4, col5 = st.columns(5,gap='medium')
+        with col1:
+            st.text(name[5])
+            st.image(poster[5])
+        with col2:
+            st.text(name[6])
+            st.image(poster[6])
+        with col3:
+            st.text(name[7])
+            st.image(poster[7])
+        with col4:
+            st.text(name[8])
+            st.image(poster[8])
+        with col5:
+            st.text(name[9])
+            st.image(poster[9])
+    elif ratings == 3:
+        name,poster = recommend(selected_movie_name)
+        col1, col2, col3, col4, col5 = st.columns(5,gap='medium')
+        with col1:
+            st.text(name[10])
+            st.image(poster[10])
+        with col2:
+            st.text(name[11])
+            st.image(poster[11])
+        with col3:
+            st.text(name[12])
+            st.image(poster[12])
+        with col4:
+            st.text(name[13])
+            st.image(poster[13])
+        with col5:
+            st.text(name[14])
+            st.image(poster[14])
+    elif ratings == 2:
+        name,poster = recommend(selected_movie_name)
+        col1, col2, col3, col4, col5 = st.columns(5,gap='medium')
+        with col1:
+            st.text(name[15])
+            st.image(poster[15])
+        with col2:
+            st.text(name[16])
+            st.image(poster[16])
+        with col3:
+            st.text(name[17])
+            st.image(poster[17])
+        with col4:
+            st.text(name[18])
+            st.image(poster[18])
+        with col5:
+            st.text(name[19])
+            st.image(poster[19])
+    elif ratings == 1:
+        name,poster = recommend(selected_movie_name)
+        col1, col2, col3, col4, col5 = st.columns(5,gap='medium')
+        with col1:
+            st.text(name[20])
+            st.image(poster[20])
+        with col2:
+            st.text(name[21])
+            st.image(poster[21])
+        with col3:
+            st.text(name[22])
+            st.image(poster[22])
+        with col4:
+            st.text(name[23])
+            st.image(poster[23])
+        with col5:
+            st.text(name[24])
+            st.image(poster[24])
+   
